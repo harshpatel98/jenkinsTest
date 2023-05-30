c67e66dd14688dde0ff036c564d6ea769aa30583
Harsh Patel <harsh.sp98@gmail.com>
2023-05-29 20:37:05 -0400

    cars.py

---
+++ cars.py
@@ -0,0 +1,12 @@
+import csv
+
+# Open the input file for reading
+with open('car_data/car_data.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    data = [row['make'] for row in reader]
+
+# Open the output file for writing
+with open('make_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['make'])  # Write the header
+    writer.writerows([[value] for value in data])  # Write the make values row by row
